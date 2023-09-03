474e8d618479a14e7b6b7276e31d9b13eed6e7b1
permCoding <ttxiom@gmail.com>
2023-09-03 22:52:44 +0500

    sumSides

---
+++ part_1/lections/lect01/ex02.js
@@ -0,0 +1,73 @@
+function ex01() {
+    // node ex01 1 2 3 4 5 6
+    // console.log(process)
+    // console.log(process.argv)
+    a = process.argv
+
+    console.log(typeof a)
+    console.log(a
+        .slice(2,)
+        .map(x => +x).filter(x => x%2!=0)
+        .reduce((a,b) => a+b, 0)
+    )
+}
+
+function ex02() {
+    // let var const
+
+    for (var i=0; i<5; ++i) {
+        console.log(i)
+    }
+    console.log(i)
+
+    const obj = {
+        "id": 1,
+        "rate": 212
+    }
+
+    obj.rate = 200
+
+    console.log(obj)
+
+    try {
+        obj = {
+            "id": 1,
+            "rate": 222
+        }    
+    } catch (error) {
+        console.error(error)
+    }
+
+    console.log('stop')
+}
+
+function ex03() {
+    // function as object
+    const func = (a, b) => {
+        return Number(a.toString() + b.toString())
+    }
+
+    const numConcat = func
+
+    console.log(numConcat(1200, 3456))
+}
+
+function ex04() { // так нельзя - анонимные не имеют ссылку
+    Array.prototype.sumSides = () => {
+        return this[0]
+    }
+    
+    let arr = [1,2,3,4]
+    console.log(arr.sumSides())
+}
+
+function ex05() { // так нельзя - анонимные не имеют ссылку
+    Array.prototype.sumSides = function () {
+        return this[0] + this[this.length-1]
+    }
+    
+    let arr = [1,2,3,4]
+    console.log(arr.sumSides())
+}
+
+ex05()
