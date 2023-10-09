b66019ca83e912e995db1cbf33a3537b6831f681
permCoding <ttxiom@gmail.com>
2023-10-09 15:25:07 +0500

    lect06

---
+++ part_1/lections/lect06-binTree-interSearch/01.js
@@ -0,0 +1,32 @@
+const log = console.log
+
+const arr = [3, 1, 5]
+let ob = { "0": 3, "1": 1, "2": 5}
+
+// log(arr[-1])
+// log(arr[arr.length-1])
+// log(arr.at(-1))
+
+log(arr.reduce((acc, cur) => acc + cur))
+log(arr.reduce((acc, cur) => acc + cur, 0))
+
+log(arr.reduce((acc, cur) => acc + cur**2)) // 29 ?
+log(arr.reduce((acc, cur) => acc + cur**2, 0)) // 35
+
+let obj = {}
+obj.id = 12
+obj["name"] = "Nike"
+log(obj)
+
+let a = [3, 1, 5]
+log(a.reduce((acc, cur, ind) => {
+    field = ind.toString()
+    acc[field] = cur
+}, {}))
+
+/*
+reduce obj  
+reduce arr  
+arr.find  
+arr.findIndex  
+*/
