5f2df09706f266540f6aaf4df13efca00c0624d8
permCoding <ttxiom@gmail.com>
2023-11-08 16:49:58 +0500

    lect-2023-11-08

--- part_1/lections/lect07-requests/01_readFile.js
+++ part_1/lections/lect07-requests/01_readFile.js
@@ -9,7 +9,7 @@ const ex_01 = () => {
 }
 
 const ex_02 = () => {
-    log(111111)/
+    log(111111)
     fs.readFile("./data/abiturs.csv", "utf-8", (error, data) => {
         log(data)
     })
@@ -21,14 +21,20 @@ const ex_03 = () => {
     fs.readFile("./data/abiturs.csv", "utf-8", (error, data) => {
         log(data)
     })
-    let pause = 3000 // 1000 ms => 1 sec
+    let pause = 500 // 500 ms => .5 sec
     setTimeout(() => log(`999999\n==> pause = ${pause} ms`), pause)
 }
 
 const ex_04 = () => {
     fs.readFile("./data/abiturs.csv", "utf-8" , (error, data) => {
         log(111111)
-        log(data)
+        fs.writeFile("call.txt", "wkqhdvq", err => {
+            log(222222)
+            fs.writeFile("call2.txt", "wkhbwjhc", err => {
+                log(data)
+                log(333333)
+            })
+        })
         log(999999)
     })
 }
@@ -43,6 +49,11 @@ const ex_05 = () => {
     let options = { encoding:"utf-8", flag: "r" }
     fs.readFile("./data/abiturs.csv", options, (error, data) => {
         if (error) throw error;
+
+        // log(111111)
+        // log(data)
+        // log(999999)
+    
         processData(data)
     })
 }
@@ -55,6 +66,14 @@ const readFilePromise = (filename) => {
             resolve(data)
         })
     })
+}
+
+try {
+    // успешно    
+} catch (error) {
+    // если были ошибки
+} finally {
+    // выполняется в любом случае
 }
 
 const start = async (filename) => {
@@ -76,8 +95,8 @@ const start = async (filename) => {
 }
 
 const ex_06 = () => {
-    // start("./data/abi_turs.csv")
-    start("./data/abiturs.csv")
+    start("./data/abi_turs.csv")
+    // start("./data/abiturs.csv")
 }
 
 ex_06()
