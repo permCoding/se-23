d84c8e0d4c49d2222918abb1964db22e56a3ac9e
permCoding <ttxiom@gmail.com>
2023-12-26 11:33:00 +0500

    closure

---
+++ examples/closures/closure.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+const createCounter = (n) => {
+    return () => {
+        return n++; // postfix n ++
+    }
+}
+
+const counter = createCounter(10)
+for (let r of [1,2,3,4,5]) {
+    console.log(counter())
+}
