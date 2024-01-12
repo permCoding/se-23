eab9172f1e60fd9049238ebd001d6d80fba8f3ae
permCoding <ttxiom@gmail.com>
2024-01-12 16:17:40 +0500

    ejs-forEach

---
+++ part_2/lections/theme00-express-html/ex_01/app-03.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { port, host } = require('./config.json');
+
+const app = express(); // app.use(express.json());
+app.use('/css', express.static('css'));
+app.set('view engine', 'ejs');
+
+let arr = require('./json/abiturs.json');
+let titles = require('./json/titles.json');
+
+app.get('/', (req, res) => {
+    res.render('index-00', { arr });
+});
+
+app.get('/fields', (req, res) => {
+    let keys = req.query.keys.split(','); console.log(keys);
+    let model = { keys, titles, arr };
+    res.render('index-03', model);
+});
+
+app.listen(port, console.log(`http://${host}:${port}`));
