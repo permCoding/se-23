6ed891e90473325ba84f73cbbef2bf72ceebbcfe
Андрей Беляков <ttxiom@gmail.com>
2024-02-26 15:29:14 +0500

    up

--- part_2/lections/theme00-express/03-rout.js
+++ part_2/lections/theme00-express/03-rout.js
@@ -5,7 +5,7 @@ const express = require('express'),
     log = console.log
 
 app.get('/', (req, res) => {
-    res.send('/')
+    res.send('/===============')
 });
 
 app.get(['/txt','/text'], (req, res) => {
