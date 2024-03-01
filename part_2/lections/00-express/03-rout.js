1239d06eb75a41b438c4fd8c91b50c0f6e2d751b
pcoding <ttxiom@gmail.com>
2024-03-01 12:21:05 +0500

    up

--- part_2/lections/00-express/03-rout.js
+++ part_2/lections/00-express/03-rout.js
@@ -33,30 +33,28 @@ app.get('/json/:num', (req, res) => { // /json/202
     res.json(obj);
 });
 
-
-const htmlError = `
-<style>
-    .error {
-        font-size: 40px;
-        height: 85%;
-        width: 100%;
-        position: fixed;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        color: #5e5e5e;
-        background-size: 25%;
-        background-repeat: no-repeat;
-    }
-</style>
-<div class='error'> Error 404 </div>
-`
+const getHtml = (msg) => {
+    return `
+        <style>
+            .error {
+                font-size: 40px;
+                height: 35%;
+                width: 100%;
+                position: fixed;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                color: #5e6e5e;
+            }
+        </style>
+        <div class='error'> ${msg} </div>`
+}
 
 app.get('/error', (req, res) => {
     log('error = 404');
     // res.status(404).end()
-    res.status(404);
-    res.send(htmlError);
+    let msg = 'incorrect address - error 404'
+    res.status(404).send(getHtml(msg));
 });
 
 app.get('/*', (req, res) => res.redirect('/error'));
--- part_2/lections/00-express/11-post.js
+++ part_2/lections/00-express/11-post.js
@@ -2,46 +2,48 @@ const express = require('express'),
     app = express(),
     HOST = 'localhost'
     PORT = 3000,
-    log = console.log
+    log = console.log,
+    abiturs = null, // для хранения массива данных
+    { writeFileSync, writeFile } = require('fs'), // для записи в json
+    decache = require('decache'); // для отмены кэширования json
 
-const { writeFileSync } = require('fs');
-const decache = require('decache');
-
-app.use((req, res, next) => {
+app.use((req, res, next) => { // middleware
     decache('./json/abiturs.json'); // отключаем кеширование для файла
+    abiturs = require('./json/abiturs.json');
     next();
 });
 
-app.use(express.json()); // обязательно добавить для распознавания объектов
+app.use(express.json()); // middleware для распознавания объектов
 
-app.get('/', (req, res) => {
-    res.send('/')
-});
+app.get('/', (req, res) => res.send('/ = = = = ') );
 
-app.get('/abiturs', (req, res) => {
-    let abiturs = require('./json/abiturs.json');
-    res.json(abiturs);
-});
+app.get('/abiturs', (req, res) => res.json(abiturs) );
 
 app.post('/abiturs', (req, res) => { // http://localhost:3000/abiturs
     log(req.body); // проверить через POSTMAN или Thunder Client
     res.status(201).end();
 });
 
-app.post('/abitursNew', (req, res) => { // http://localhost:3000/abiturs
-    let abiturs = require('./json/abiturs.json');
-    abiturs.push(req.body);
+app.post('/abitursNew', (req, res) => { // http://localhost:3000/abitursNew
+    abiturs.push(req.body); // объект от клиента
     res.json(abiturs);
 });
 
-app.post('/abitursSave', (req, res) => { // http://localhost:3000/abiturs
-    let abiturs = require('./json/abiturs.json');
+app.post('/abitursSaveSync', (req, res) => { // http://localhost:3000/abitursSaveSync
     abiturs.push(req.body);
     writeFileSync(
         './json/abiturs.json', 
-        JSON.stringify(abiturs, null, 4), {encoding: 'utf8'}
+        JSON.stringify(abiturs, null, 4), 
+        { encoding: 'utf8' }
     );
     res.json(abiturs);
+});
+
+app.post('/abitursSave', (req, res) => { // http://localhost:3000/abitursSave
+    abiturs.push(req.body);
+    let filename = './json/abiturs.json';
+    let jsonStr = JSON.stringify(abiturs, null, 4);
+    writeFile(filename, jsonStr, 'utf8', () => res.json(abiturs));
 });
 
 app.listen(PORT, HOST, () => log(`http://${HOST}:${PORT}/`));
