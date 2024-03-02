4e219d8aa6e0f508f2b1e0c5f643e77d60ee6d1d
Андрей Беляков <ttxiom@gmail.com>
2024-03-02 18:16:34 +0500

    hbs

--- part_2/lections/02-express-html/ex_00_hbs/index.js
+++ part_2/lections/02-express-html/ex_00_hbs/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 // настройка приложения
 const app = express(); // app.use(express.json());
 app.use('/css', express.static('css'));
+app.use('/images', express.static('images'));
 app.set('view engine', 'hbs'); // npm i hbs
 
 // паттерн проектирования MVC
@@ -14,19 +15,19 @@ const arr = require('./json/abiturs.json');
 // controllers - обработчики событий
 app.get('/', (req, res) => {
     let model = { arr };
-    res.render('index-00', model); // view - представление
+    res.render('index', model); // view - представление
 });
 
 app.get('/filter', (req, res) => {
     let { query } = req;
     let model = { arr: arr.filter(x => x.city == query.city) };
-    res.render('index-00', model);
-});
+    res.render('index', model);
+}); // http://localhost:3000/filter?city=Кунгур
 
 app.get('/:limit', (req, res) => {
     let model = { arr: arr.slice(0, req.params.limit) };
-    res.render('index-00', model);
-});
+    res.render('index', model);
+}); // http://localhost:3000/8
 
 // запуск приложения
 app.listen(3000, console.log('http://localhost:3000'));
