4997ddad45909e8dda43d1ffebae29d6778e5d37
Андрей Беляков <ttxiom@gmail.com>
2024-03-13 12:11:19 +0500

    lab-01

--- part_2/labrabs/01-rest-wb/groupB/task01.js
+++ part_2/labrabs/01-rest-wb/groupB/task01.js
@@ -6,7 +6,7 @@ let strParams = "ab_testing=false&appType=1&curr=rub&dest=12358373&page=1&query=
 let link = `${host}${strParams}`;
 
 (async () => {
-    let resp = await fetch(link, { headers, "body": null, "method": "GET" });
+    let resp = await fetch(link, { headers, "method": "GET" });
     let json = await resp.json();
     let products = json.data.products;
     console.log(JSON.stringify(products, null, 4));
