f0c0d7d5046f7cde7c836427c0e6b23b76962dcc
Андрей Беляков <ttxiom@gmail.com>
2024-04-07 14:52:57 +0500

    up

--- part_2/lections/02-express-html/ex_02_ejs_post_rout/routers/rout_new.js
+++ part_2/lections/02-express-html/ex_02_ejs_post_rout/routers/rout_new.js
@@ -12,12 +12,11 @@ router.post('/new', (req, res) => {
     console.log(req.body); // to control the values
     // let maxId = abiturs.length; // ver_1
     // let maxId = Math.max(...abiturs.map(x => +x.id)); // ver_2
-    
-    let maxId = global.abiturs
-        .reduce((prevMax, curr) => Math.max(prevMax, +curr.id), 0); // ver_3
-    
-    let newAbit = Object
-        .assign(
+    // let maxId = global.abiturs
+    //     .reduce((prevMax, curr) => Math.max(prevMax, +curr.id), 0); // ver_3
+    let maxId = +global.abiturs.at(-1).id; // ver_4
+
+    let newAbit = Object.assign(
             { "id": maxId+1 }, 
             req.body,
             { "birthDate": "2002-07-30", "city": "Оса" } // заглушка
